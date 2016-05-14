e7634db39bd747834d047905bd9828f9c8d3e3d8
Josué Us <estuardo1915@hotmail.com>
2016-05-13 20:48:49 -0600

    SJ.135.2
    Terminado!

--- app/src/main/java/me/ujosue/starbucks/MainActivity.java
+++ app/src/main/java/me/ujosue/starbucks/MainActivity.java
@@ -1,13 +1,33 @@
 package me.ujosue.starbucks;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    //Llama el controlador en donde escribimos la cantidad de tazas deseadas
+    private EditText tazas = null;
+    //Llama al boton para pedir
+    private Button btnPedir = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tazas = (EditText) findViewById(R.id.etPedir);
+        btnPedir = (Button) findViewById(R.id.btnPedir);
+        btnPedir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Declara que queremos ir a la vista de factura
+                Intent intent = new Intent(MainActivity.this, Factura.class);
+                //Le enviamos la cantidad de tazas que se pidieron
+                intent.putExtra("tazas", tazas.getText().toString());
+                //Nos manda a la vista de la factura
+                startActivity(intent);
+            }
+        });
     }
 }
